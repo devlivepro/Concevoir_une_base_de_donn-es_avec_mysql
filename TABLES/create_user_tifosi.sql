bd7e94ade3f125d01f619003237c9cd62d9919dd
devlivepro <155884158+devlivepro@users.noreply.github.com>
2024-04-19 14:28:33 +0200

    Add news tables

---
+++ TABLES/create_user_tifosi.sql
@@ -0,0 +1,4 @@
+-- Identifiant et mots de passe par défaut : tifosi
+
+GRANT ALL PRIVILEGES ON *.* TO `tifosi`@`localhost` IDENTIFIED BY PASSWORD '*C15BA69C8917D768C4C0DC80D09E51EF53E4EDC5' WITH GRANT OPTION;
+
